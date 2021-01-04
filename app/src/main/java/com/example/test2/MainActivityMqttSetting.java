311bb8ec883c0916b05548a91b1b6b72a2b5ad65
DSPG\avik <avi.keren@dspg.com>
2021-01-04 16:23:18 +0200

    adding days and sprinkels table for feeding the starting times

--- app/src/main/java/com/example/test2/MainActivityMqttSetting.java
+++ app/src/main/java/com/example/test2/MainActivityMqttSetting.java
@@ -17,56 +17,57 @@ import android.widget.TimePicker;
 
 public class MainActivityMqttSetting extends AppCompatActivity
 {
-   //RadioGroup radioGroup;
-    RadioButton[] rbsprinklers = new RadioButton[3];
-    RadioButton[] rbdayofweek = new RadioButton [7];
+    TextView[] tvTimessp1 = new TextView[7];
+    TextView[] tvTimessp2 = new TextView[7];
+    TextView[] tvTimessp3 = new TextView[7];
 
     TimePickerDialog timePickerDialog;
-   TextView chooseTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mqtt_setting);
+        String spname;
 
-        rbsprinklers[0]=(RadioButton) findViewById(R.id.radio_one_spr);
-        rbsprinklers[1]=(RadioButton) findViewById(R.id.radio_two_spr);
-        rbsprinklers[2]=(RadioButton) findViewById(R.id.radio_three_spr);
-
-        rbdayofweek[0]=(RadioButton) findViewById(R.id.rbDayOfWeek0);
-        rbdayofweek[1]=(RadioButton) findViewById(R.id.rbDayOfWeek1);
-        rbdayofweek[2]=(RadioButton) findViewById(R.id.rbDayOfWeek2);
-        rbdayofweek[3]=(RadioButton) findViewById(R.id.rbDayOfWeek3);
-        rbdayofweek[4]=(RadioButton) findViewById(R.id.rbDayOfWeek4);
-        rbdayofweek[5]=(RadioButton) findViewById(R.id.rbDayOfWeek5);
-        rbdayofweek[6]=(RadioButton) findViewById(R.id.rbDayOfWeek6);
-
-        chooseTime=findViewById(R.id.editTextTimeSprinkler);
-        chooseTime.setText("Set Time");
-    }
-
-    public void SprinklerscheckButton(View view)
-    {
-     for (int i=0 ; i<3; i++)
-     {
-      if (rbsprinklers[i].isChecked())
-             Log.w("radio", "Sprinkler: " + Integer.toString(i));
-     }
-    }
-
-    public void WeekDaysCheckButton(View view)
-    {
         for (int i=0 ; i<7; i++)
         {
-            if (rbdayofweek[i].isChecked())
-                Log.w("radio", "Day of week: " + Integer.toString(i));
+         spname="textViewsp1d"+Integer.toString(i);
+         int id = getResources().getIdentifier(spname, "id", getPackageName());
+         if (id != 0 )
+            {
+             tvTimessp1[i] = (TextView) findViewById(id);
+             tvTimessp1[i].setText("Set Time");
+            }
+        }
+
+        for (int i=0 ; i<7; i++)
+        {
+            spname="textViewsp2d"+Integer.toString(i);
+            int id = getResources().getIdentifier(spname, "id", getPackageName());
+            if (id != 0 )
+            {
+                tvTimessp2[i] = (TextView) findViewById(id);
+                tvTimessp2[i].setText("Set Time");
+            }
+        }
+
+        for (int i=0 ; i<7; i++)
+        {
+            spname="textViewsp3d"+Integer.toString(i);
+            int id = getResources().getIdentifier(spname, "id", getPackageName());
+            if (id != 0 )
+            {
+                tvTimessp3[i] = (TextView) findViewById(id);
+                tvTimessp3[i].setText("Set Time");
+            }
         }
     }
 
     public void TimeForSprinklersStart(View view)
     {
      TimePickerDialog mTimePicker;
+     TextView sptime = (TextView) findViewById(view.getId());
 
      Log.w("clicking","aaa");
      mTimePicker = new TimePickerDialog(MainActivityMqttSetting.this, new TimePickerDialog.OnTimeSetListener()
@@ -74,9 +75,10 @@ public class MainActivityMqttSetting extends AppCompatActivity
          @Override
          public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
          {
-          chooseTime.setText( selectedHour + ":" + selectedMinute);
+          sptime.setText( selectedHour + ":" + selectedMinute);
          }
       }, 24, 0,true);
+
      mTimePicker.setTitle("Select Time");
      mTimePicker.show();
     }
